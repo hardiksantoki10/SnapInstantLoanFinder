2a7956dea45e62824d06ded1724948ce02b9f8d1
PrincePoriya <poriyaprince82@gmail.com>
2024-01-11 23:33:39 +0530

    Changelog (11-01-2024): 1. Slider added

---
+++ app/src/main/java/com/snap/instant/loan/finder/activity/adapter/SliderAdapter.kt
@@ -0,0 +1,49 @@
+package com.snap.instant.loan.finder.activity.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.snap.instant.loan.finder.R
+import com.snap.instant.loan.finder.activity.adapter.SliderAdapter.SliderViewHolder
+
+class SliderAdapter internal constructor( val sliderItems: ArrayList<Int>, val viewPager2: ViewPager2) :
+    RecyclerView.Adapter<SliderViewHolder>() {
+    @NonNull
+    override fun onCreateViewHolder(@NonNull parent: ViewGroup, viewType: Int): SliderViewHolder {
+        return SliderViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.slide_item_container, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(@NonNull holder: SliderViewHolder, position: Int) {
+        holder.setImage(sliderItems[position])
+        if (position == sliderItems.size - 2) {
+            viewPager2.post(runnable)
+        }
+    }
+    override fun getItemCount(): Int {
+        return sliderItems.size
+    }
+    inner class SliderViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView
+
+        init {
+            imageView = itemView.findViewById(R.id.imageSlide)
+        }
+        fun setImage(sliderItems: Int) {
+//use glide or picasso in case you get image from internet
+            imageView.setImageResource(sliderItems)
+        }
+    }
+
+    private val runnable = Runnable {
+        sliderItems.addAll(sliderItems)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/snap/instant/loan/finder/activity/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/snap/instant/loan/finder/activity/ui/fragment/HomeFragment.kt
@@ -1,23 +1,28 @@
 package com.snap.instant.loan.finder.activity.ui.fragment
 
+
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Orientation
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
 import com.snap.instant.loan.finder.R
+import com.snap.instant.loan.finder.activity.adapter.SliderAdapter
 import com.snap.instant.loan.finder.databinding.FragmentHomeBinding
 import com.snap.instant.loan.finder.databinding.HomeListProviderItemBinding
+import kotlin.math.abs
+
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private val sliderHandler: Handler = Handler(Looper.getMainLooper())
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,9 +42,50 @@ class HomeFragment : Fragment() {
     private fun initList() {
         binding.rvLoanProviderList.layoutManager = LinearLayoutManager(requireContext())
         binding.rvLoanProviderList.adapter = HomeListAdapter()
+
+
+        val sliderItems = ArrayList<Int>()
+        sliderItems.add(R.drawable.ic_slider_1)
+        sliderItems.add(R.drawable.ic_slider_2)
+        sliderItems.add(R.drawable.ic_slider_3)
+        sliderItems.add(R.drawable.ic_slider_4)
+
+        binding.viewPagerHorizontal.setAdapter(
+            SliderAdapter(
+                sliderItems,
+                binding.viewPagerHorizontal
+            )
+        )
+
+        binding.viewPagerHorizontal.clipToPadding = false
+        binding.viewPagerHorizontal.clipChildren = false
+        binding.viewPagerHorizontal.setOffscreenPageLimit(3)
+        binding.viewPagerHorizontal.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+
+        val compositePageTransformer = CompositePageTransformer()
+        compositePageTransformer.addTransformer(MarginPageTransformer(18))
+        compositePageTransformer.addTransformer { page, position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.90f + r * 0.1f
+        }
+
+        binding.viewPagerHorizontal.setPageTransformer(compositePageTransformer)
+        binding.viewPagerHorizontal.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                sliderHandler.removeCallbacks(sliderRunnable)
+                sliderHandler.postDelayed(sliderRunnable, 2000) // slide duration 2 seconds
+            }
+        })
     }
 
+    private val sliderRunnable = Runnable { binding.viewPagerHorizontal.currentItem += 1 }
+
+
 }
+
+
 
 class HomeListAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
