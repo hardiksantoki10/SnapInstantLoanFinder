3640e82e255dbbf1f0d4f6eaf71dc4f5d034e16b
Zennaxx <zennaxxhardik@gmail.com>
2024-01-18 21:17:14 +0530

    ---- Calculator Fragment

--- app/src/main/java/com/snap/instant/loan/finder/activity/ui/fragment/CalcFragment.kt
+++ app/src/main/java/com/snap/instant/loan/finder/activity/ui/fragment/CalcFragment.kt
@@ -17,7 +17,7 @@ class CalcFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_calculator, container, false)
     }
 
 }
